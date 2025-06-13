9275daa9fec73464d41098d15a7f8a8807e9e8de
Be Cappy! <thien.nglee@gmail.com>
2025-06-14 06:22:33 +0700

    Upload

--- src/components/Header/Search/SearchInput.jsx
+++ src/components/Header/Search/SearchInput.jsx
@@ -1,9 +1,10 @@
-import { forwardRef } from 'react';
+import { forwardRef, useState } from 'react';
 import styles from '../../../style/components/Header/Search/SearchInput.module.scss';
 import { setQuery } from '../../../redux/slices/searchSlice';
 import { useTranslation } from 'react-i18next';
 
 const SearchInput = forwardRef(({ handleSearch, query, dispatch }, ref) => {
+    const [suggestions, setSuggestions] = useState(false);
     const { t } = useTranslation();
 
     return (
@@ -14,10 +15,18 @@ const SearchInput = forwardRef(({ handleSearch, query, dispatch }, ref) => {
                     ref={ref}
                     placeholder={t('header.inputPlaceholder')}
                     value={query}
-                    onChange={(e) => dispatch(setQuery(e.target.value))}
+                    onChange={(e) => {
+                        dispatch(setQuery(e.target.value));
+                        setSuggestions((prev) => !prev);
+                    }}
                     onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
                 />
             </form>
+            {suggestions && (
+                <ul>
+                    <li>hehe</li>
+                </ul>
+            )}
         </section>
     );
 });
