f58a9411272a99be0a87086253a4a77c16e76a4d
Be Cappy! <thien.nglee@gmail.com>
2025-06-22 14:52:21 +0700

    Back to simple

--- src/components/Shared/Overlay/Overlay.jsx
+++
@@ -1,7 +0,0 @@
-import styles from '../../../styles/components/Shared/Overlay/Overlay.module.scss';
-
-function Overlay() {
-    return <section className={styles.overlay}></section>;
-}
-
-export default Overlay;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,7 @@
 import styles from '../../styles/pages/HomePage/HomePage.module.scss';
 import { HomePagePopularTypesTitle, HomePageMusicContent } from './HomePageImports';
-import { toast } from 'react-toastify';
 
 function HomePage() {
-    toast.success('🎉 Chào mừng bạn đến với Youtube Clone!');
     return (
         <section className={styles.homePage}>
             <HomePagePopularTypesTitle />
